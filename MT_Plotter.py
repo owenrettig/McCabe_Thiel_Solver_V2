48c9342482bb970136e4f32026cce2b8a1b29790
owenrettig <rettigod@rose-hulman.edu>
2026-02-06 02:36:00 -0500

    Add files via upload

--- MT_Plotter.py
+++ MT_Plotter.py
@@ -10,7 +10,7 @@ data = pd.read_excel("VLE_Data.xlsx", sheet_name=1)
 data.iloc[0, 1] = 1e-10
 
 # Assign data from excel to values
-xcalc = np.linspace(0,1,500)
+xcalc = np.linspace(0,1,5000)
 xdata = data["x"].values
 ydata = data["y"].values
 f = data.iloc[0,3]
@@ -20,6 +20,7 @@ boilup_ratio = data.iloc[3,3]
 xD = data.iloc[4,3]
 xB = data.iloc[5,3]
 q = data.iloc[6,3]
+
 
 
 ######################################
@@ -40,10 +41,6 @@ else:
     Vbar = L + D
     V = Vbar + f
     Lbar = L
-    
-
-    
-
     
 # regress vle data
 coeffs = np.polyfit(xdata,ydata, deg = 8)
@@ -104,6 +101,34 @@ yop = combined_line(xcalc)
 
 ######################################
 
+# create stage lines 
+def stage_stepper():
+    max_stages = 1000
+    xs = np.zeros(max_stages)
+    ys = np.zeros(max_stages)
+    xs[0] = xD
+    ys[0] = xD
+    i = 1
+
+    while True:
+        g = lambda x: p(x) - (ys[i-1])
+        xresult = root_scalar(g, x0 = xs[i-1], x1 = xs[i-1] + 0.1)
+        xs[i] = xresult.root
+        ys[i] = p(xs[i])
+        xs[i+1] = xs[i]
+        ys[i+1] = combined_line(xs[i+1])
+        if ys[i+1] < xB:
+            break
+        if i > 996:
+            print("Separation not possible")
+            break
+        i += 2
+    return xs[:i+2], ys[:i+2]
+
+xs, ys = stage_stepper()
+
+
+######################################
 
 #plot the chart
 def plot_chart():
@@ -111,6 +136,7 @@ def plot_chart():
     plt.plot(xdata,ydata,"r*")
     plt.plot(xfit,yfit,"k")
     plt.plot(xcalc,yop)
+    plt.plot(xs,ys)
     plt.plot([0,1],[0,1])
     if q != 1 and q !=0 :
         plt.plot(xcalc,yq)
@@ -120,6 +146,7 @@ def plot_chart():
     plt.ylim([0,1])
     plt.xlabel("x_1")
     plt.ylabel("y_1")
+    plt.grid(True)
     plt.show()
    
 plot_chart()
