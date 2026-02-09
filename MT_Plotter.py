4d129932d79c05dbcb9b37af84ffa246c1185b4a
owenrettig <rettigod@rose-hulman.edu>
2026-02-09 17:02:41 -0500

    Updated Plotter

--- MT_Plotter.py
+++ MT_Plotter.py
@@ -7,7 +7,6 @@ os.system('cls')
 
 # Import data from excel
 data = pd.read_excel("VLE_Data.xlsx", sheet_name=1)
-data.iloc[0, 1] = 1e-10
 
 # Assign data from excel to values
 xcalc = np.linspace(0,1,5000)
@@ -21,32 +20,49 @@ xD = data.iloc[4,3]
 xB = data.iloc[5,3]
 q = data.iloc[6,3]
 
-
-
 ######################################
 
 # Calculate D, B, L, V, Lbar, Vbar
-B = (z * f - xD * f)/(xB - xD)
-D = f - B
-L = reflux_ratio * D
-if q == 1:
-    V = L + D
-    Vbar = V
-    Lbar = L + f
-elif q != 0:
-    V = L + D
-    Vbar = V + (1 - q) * f
-    Lbar = L + q * f
+if reflux_ratio != 0:
+    B = (z * f - xD * f)/(xB - xD)
+    D = f - B
+    L = reflux_ratio * D
+    if q == 1:
+        V = L + D
+        Vbar = V
+        Lbar = L + f
+    elif q != 0:
+        V = L + D
+        Vbar = V + (1 - q) * f
+        Lbar = L + q * f
+    else:
+        Vbar = L + D
+        V = Vbar + f
+        Lbar = L
 else:
-    Vbar = L + D
-    V = Vbar + f
-    Lbar = L
-    
+    B = (z * f - xD * f)/(xB - xD)
+    D = f - B
+    Vbar = boilup_ratio * D
+    if q == 1:
+        Lbar = Vbar + B
+        V = Vbar 
+        L = Lbar - f
+    elif q == 0:
+        Lbar = Vbar + B
+        L = Lbar
+        V = Vbar + f
+    else:
+        Lbar = Vbar + B
+        L = Lbar - q*f
+        V = Vbar + (1-q)*f
+
+######################################
+   
 # regress vle data
-coeffs = np.polyfit(xdata,ydata, deg = 8)
+coeffs = np.polyfit(xdata,ydata, deg = 10)
 y_pred = np.polyval(coeffs,xdata)
 p = np.poly1d(coeffs)
-xfit = np.linspace(1e-3,1,500)
+xfit = np.linspace(0,1,5000)
 yfit = p(xfit)
 
 ######################################
@@ -78,7 +94,7 @@ if reflux_ratio != 0:
         result = root_scalar(h , x0 = z)
         fintercept = result.root
     # handle situation where q is not 1 
-    if q != 1 and q !=0 :
+    elif q != 1:
         h = lambda x: (tslope - fslope) * x + (tintercept - fintercept)
         result = root_scalar(h , x0 = z)
         fintercept = result.root
@@ -86,7 +102,22 @@ if reflux_ratio != 0:
     bslope = ((tslope * fintercept + tintercept) - xB) / (fintercept - xB)
     bintercept = -(bslope - 1) * xB
 else:
-    tslope = 1
+    # top slope
+    bslope = Lbar / Vbar 
+    bintercept = -(bslope - 1) * xB
+    #hande situation where q is equal to 0
+    if q == 0:
+        k = lambda x: bslope * x + bintercept - z
+        result = root_scalar(k, x0 = z)
+        fintercept = result.root
+    # handle situation where q is not 1
+    if q != 1:
+        k = lambda x: (bslope - fslope) * x + (bintercept - fintercept)
+        result = root_scalar(k, x0 = z)
+        fintercept = result.root
+    # top slope
+    tslope = ((bslope * fintercept + bintercept) - xD) / (fintercept - xD)
+    tintercept = -(tslope - 1) * xD
 
 ######################################
 
@@ -112,12 +143,14 @@ def stage_stepper():
 
     while True:
         g = lambda x: p(x) - (ys[i-1])
-        xresult = root_scalar(g, x0 = xs[i-1], x1 = xs[i-1] + 0.1)
+        xresult = root_scalar(g, x0 = xs[i-1], x1 = xs[i-1] - 0.1)
         xs[i] = xresult.root
         ys[i] = p(xs[i])
         xs[i+1] = xs[i]
         ys[i+1] = combined_line(xs[i+1])
         if ys[i+1] < xB:
+            n = (i+1)/2
+            print(f"{n} Stages required for separation")
             break
         if i > 996:
             print("Separation not possible")
@@ -128,25 +161,42 @@ def stage_stepper():
 xs, ys = stage_stepper()
 
 
-######################################
-
 #plot the chart
 def plot_chart():
-    plt.figure("Plotted Line")
-    plt.plot(xdata,ydata,"r*")
-    plt.plot(xfit,yfit,"k")
-    plt.plot(xcalc,yop)
-    plt.plot(xs,ys)
-    plt.plot([0,1],[0,1])
-    if q != 1 and q !=0 :
-        plt.plot(xcalc,yq)
+    fig = plt.figure("Plotted Line")
+    ax = plt.gca()
+    
+    # Set background colors (MATLAB dark mode)
+    fig.patch.set_facecolor('#121212')  # outer figure background
+    ax.set_facecolor("#1c1c1c")  # plot area background
+    
+    # MATLAB default color cycle (same colors work in dark mode)
+    matlab_colors = ['#0072BD', '#D95319', '#EDB120', '#7E2F8E', '#77AC30', '#4DBEEE', '#A2142F','#ffffff']
+    
+    plt.plot(xfit, yfit, color=matlab_colors[0], linewidth=1.5)
+    plt.plot(xcalc, yop, color=matlab_colors[3], linewidth=1.5)
+    plt.plot(xs, ys, color=matlab_colors[4], linewidth=1)
+    plt.plot([0,1], [0,1], color=matlab_colors[2], linewidth=1)
+    plt.plot(xdata, ydata, "*", color=matlab_colors[7], markersize=3, alpha = 0.3)
+    
+    if q != 1 and q != 0:
+        plt.plot(xcalc, yq, color=matlab_colors[1], linewidth=1)
     else:
-        plt.plot(xq,yq)
+        plt.plot(xq, yq, color=matlab_colors[1], linewidth=1)
+    
     plt.xlim([0,1])
     plt.ylim([0,1])
-    plt.xlabel("x_1")
-    plt.ylabel("y_1")
-    plt.grid(True)
+    plt.xlabel("$x_1$", fontsize=11, color='white')
+    plt.ylabel("$y_1$", fontsize=11, color='white')
+    
+    # Style axes and ticks (MATLAB dark mode)
+    ax.tick_params(colors='white', which='both', labelsize=10)
+    ax.spines['top'].set_color('#4d4d4d')
+    ax.spines['bottom'].set_color('#4d4d4d')
+    ax.spines['left'].set_color('#4d4d4d')
+    ax.spines['right'].set_color('#4d4d4d')
+    
+    plt.grid(True, color='#404040', linestyle='-', linewidth=0.5)
     plt.show()
    
 plot_chart()
